dab43e3746c319726b3ef0c8d35e713cdc94bd39
Sebastian Döll <sebastian@katallaxie.me>
2024-05-01 20:27:54 +0000

    wip: model validation

--- internal/api/models/system.go
+++ internal/api/models/system.go
@@ -9,9 +9,12 @@ import (
 
 // System ...
 type System struct {
-	ID          uuid.UUID `json:"id" gorm:"type:uuid;default:gen_random_uuid()"`
-	Name        string    `json:"name" gorm:"unique"`
-	Description string    `json:"description"`
+	// ID is the unique identifier for the system.
+	ID uuid.UUID `json:"id" gorm:"type:uuid;default:gen_random_uuid()"`
+	// Name is the name of the system.
+	Name string `json:"name" gorm:"unique" validate:"required,min=3,max=128"`
+	// Description is the description of the system.
+	Description string `json:"description" validate:"max=1024"`
 
 	// Operator is the operator this is associated with this system to operate.
 	Operator   Operator   `json:"operator" gorm:"foreignKey:OperatorID"`
