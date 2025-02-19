19ec6b53ebbe1b10b33fd2fa74631c45a4c32c70
Sebastian Döll <sebastian@katallaxie.dev>
2025-02-19 12:27:03 +0000

    chore: update devcontainers

--- scripts/postCreateCommand.sh
+++ scripts/postCreateCommand.sh
@@ -2,7 +2,7 @@
 # This script is executed after the creation of a new project.
 
 sudo apt-get update
-sudo apt-get install libxml2-dev libxslt1-dev
+sudo apt-get install -y libxml2-dev libxslt1-dev
 
 go install github.com/nats-io/natscli/nats@latest
 go install github.com/nats-io/nats-top@latest
